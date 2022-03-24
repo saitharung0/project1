ce0ebe1d9c89ec24593161a2ca9aa1137c018ca7
saitharung0 <95485208+saitharung0@users.noreply.github.com>
2022-03-24 19:36:11 +0530

    this project is about adding an profile to instagram

---
+++ instagram_profile.py
@@ -0,0 +1,15 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========== RESTART: C:/Users/Admin/AppData/Local/Programs/Python/Python39/instagram_profile.py =========
+Traceback (most recent call last):
+  File "C:/Users/Admin/AppData/Local/Programs/Python/Python39/instagram_profile.py", line 1, in <module>
+    import requests
+ModuleNotFoundError: No module named 'requests'
+>>> 
+========== RESTART: C:/Users/Admin/AppData/Local/Programs/Python/Python39/instagram_profile.py =========
+Traceback (most recent call last):
+  File "C:/Users/Admin/AppData/Local/Programs/Python/Python39/instagram_profile.py", line 1, in <module>
+    import requests
+ModuleNotFoundError: No module named 'requests'
+>>> 
